91d38e7320287871f8a4c10cfacaeb150752bc6a
Noah Liu <noahxp@gmail.com>
2017-06-03 22:10:20 +0800

    Update index.js

--- SnsToSlack/index.js
+++ SnsToSlack/index.js
@@ -9,14 +9,7 @@ exports.handler = function(event, context) {
     console.log(JSON.stringify(event, null, 2));
     console.log('From SNS:', event.Records[0].Sns.Message);
 
-    /*
-    var postData = {
-        "channel": "#aws-sns",
-        "username": "AWS SNS via Lamda :: DevQa Cloud",
-        "text": "*" + event.Records[0].Sns.Subject + "*",
-        "icon_emoji": ":aws:"
-    };
-    */
+  
     var postData = {
         "username": "AWS SNS",
         "text": event.Records[0].Sns.Subject,
@@ -87,7 +80,7 @@ exports.handler = function(event, context) {
     };
 
     var req = https.request(options, function(res) {
-      res.setEncoding('utf8');
+      res.setEncoding('utf-8');
       res.on('data', function (chunk) {
         context.done(null);
       });
